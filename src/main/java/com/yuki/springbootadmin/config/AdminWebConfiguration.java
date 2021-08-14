57a1b76ed5891efb3eed95d0092ae664cdcf1928
YuKino-NPH <1615937569@qq.com>
2021-08-14 15:04:02 +0800

    FirstCommit1.2

---
+++ src/main/java/com/yuki/springbootadmin/config/AdminWebConfiguration.java
@@ -0,0 +1,14 @@
+package com.yuki.springbootadmin.config;
+
+import com.yuki.springbootadmin.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AdminWebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+       registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**").excludePathPatterns("/","/login","/css/**","/fonts/**","/images/**","/js/**");
+    }
+}
--- src/main/java/com/yuki/springbootadmin/controller/LoginController.java
+++ src/main/java/com/yuki/springbootadmin/controller/LoginController.java
@@ -1,12 +1,33 @@
 package com.yuki.springbootadmin.controller;
 
+import com.yuki.springbootadmin.bean.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class LoginController {
-    @GetMapping(value={"/","/login"})
-    public String indexPage(){
+    @GetMapping(value = {"/", "/login"})
+    public String indexPage() {
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String main(User user, HttpSession session, Model model) {
+        if (StringUtils.hasLength(user.getUserName())) {
+            session.setAttribute("loginUser", user);
+            return "redirect:main.html";
+        }
+        model.addAttribute("msg", "账号或密码错误");
+        return "/login";
+    }
+
+    @GetMapping("/main.html")
+    public String goMain(HttpSession session) {
+        return "main";
     }
 }
